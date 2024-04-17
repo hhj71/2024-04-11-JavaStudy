1fb76def8250819cb0e70f987cdd7023690444f8
sist111 <sist111@DESKTOP-N1E8SO0>
2024-04-17 11:55:02 +0900

    0417

--- Chapter03_제어문_2/src/GameView.java
+++ Chapter03_제어문_2/src/GameView.java
@@ -6,11 +6,11 @@ public class GameView extends JPanel {
          int y=500;
 	public GameView() 
 	{ 
-		back=Toolkit.getDefaultToolkit().getImage("C:\\javaDev\\back.jpg");
-		horse=Toolkit.getDefaultToolkit().getImage("C:\\javaDev\\horse.gif");
+		back=Toolkit.getDefaultToolkit().getImage("C:\\javaDev\\back.jpg");  // 파일 경로에 파일을 넣어두어야 한다. 
+		horse=Toolkit.getDefaultToolkit().getImage("C:\\javaDev\\horse.gif"); // 파일 경로에 파일을 넣어두어야 한다. 
 	}
 
-	@Override
+	@Override                                                               // 코드창에서 오른쪽 마우스 클릭 source-> override -> paint 체크
 	public void paint(Graphics g) {
 		g.drawImage(back, 0, 0, getWidth(), getHeight(), this);
 		g.drawImage(horse, x, y, 120, 80, this);
