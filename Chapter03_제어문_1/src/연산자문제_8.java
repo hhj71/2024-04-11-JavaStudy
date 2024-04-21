59f4513654b32fcb114e38d528bbd66908f98236
Hyejin <166673446+hhj71@users.noreply.github.com>
2024-04-21 16:45:32 +0900

    0421

--- Chapter03_제어문_1/src/연산자문제_8.java
+++ Chapter03_제어문_1/src/연산자문제_8.java
@@ -2,7 +2,7 @@
 public class 연산자문제_8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
 		 int num1=10, num2=10;
 		    System.out.printf("%d \n", ++num1);// 출력전에 증가
 		    System.out.printf("%d \n", num1);
