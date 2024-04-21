f56b8a4b411f74558a82c8338838169f5bc9d2e0
Hyejin <166673446+hhj71@users.noreply.github.com>
2024-04-21 16:44:25 +0900

    0421

--- Chapter03_제어문_1/src/연산자문제_7.java
+++ Chapter03_제어문_1/src/연산자문제_7.java
@@ -2,7 +2,7 @@
 public class 연산자문제_7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
 		 int num1=10, num2=10;   
 	        int a, b;   
 	        num1 = num1 + 1;
