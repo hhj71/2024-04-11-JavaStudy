d0a2f5f315701583474ed691678374bb6b2b7853
Hyejin <166673446+hhj71@users.noreply.github.com>
2024-04-21 16:37:13 +0900

    Add files via upload

---
+++ Chapter03_제어문_1/src/연산자문제13.java
@@ -0,0 +1,17 @@
+/*
+ * 두 개의 정수를 입력 받아서 첫째 수를 둘째 수로 나눈 나머지를 
+ * 출력하는 프로그램을 작성해 보자.
+ */
+import java.util.Scanner;
+public class 연산자문제13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Scanner scan = new Scanner(System.in);
+         System.out.print("두 개의 정수를 입력하시오(10 20):");
+         int num1 = scan.nextInt();
+         int num2 = scan.nextInt();
+         System.out.printf("%d %% %d = %d\n", num1, num2, num1%num2);
+	}
+
+}
---
+++ Chapter03_제어문_1/src/연산자문제_6.java
@@ -0,0 +1,37 @@
+
+
+public class 연산자문제_6 {
+
+	public static void main(String[] args) {
+		
+		int num1=10, num2=10;   
+	       int a, b;   
+	       a = ++num1;
+	       b = num2++;
+
+	       System.out.printf("%d, %d \n", b, num2);
+
+	}
+              /* 결과값은
+               * 10, 11
+               */
+}
+/*
+ *     int num1=10, num2=10;   
+       int a, b;   
+       a = ++num1;
+           --
+           => 전치 연산자 => 먼저 증가
+         -- 증가된 값을 대입 
+       a=11 num1=11
+       b = num2++;
+       --------
+         1 => 후치연산자 => 대입
+           ------
+             2 => 증가 
+
+       System.out.printf("%d, %d \n", b, num2);
+                                         =====  11
+                                     === 10 
+
+ */
