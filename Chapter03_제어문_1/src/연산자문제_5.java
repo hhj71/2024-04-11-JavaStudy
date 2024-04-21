b52c655db6e117958e2c7e4c50c731acf52407a5
Hyejin <166673446+hhj71@users.noreply.github.com>
2024-04-21 16:43:54 +0900

    0421

--- Chapter03_제어문_1/src/연산자문제_5.java
+++ Chapter03_제어문_1/src/연산자문제_5.java
@@ -3,7 +3,7 @@ public class 연산자문제_5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 int num1=10;   
+		 int num1=10;    
 	      System.out.printf("%d \n", num1);
 	      num1++;
 	      System.out.printf("%d \n", num1);
