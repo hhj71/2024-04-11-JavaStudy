ca176dd175e04a8f2ca3572d3fd344d8ce4e4f1c
sist111 <sist111@DESKTOP-N1E8SO0>
2024-04-22 17:39:43 +0900

    0422

---
+++ Chapter03_제어문_while_1__1/src/문제_1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class 문제_1 {
+	 
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("두자리 정수 입력:");
+	}
+} 
+
---
+++ Chapter03_제어문_while_1__1/src/문제_8.java
@@ -0,0 +1,24 @@
+
+public class 문제_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+/*
+ * 반복문 => 용도 따라서 사용
+ *     
+ *     ---------------
+ *     for: 반복의 횟수가 지정 =>
+ *     while: 반복횟수가 없는 경우 => 데이터 관리
+ *     ---------------------------------
+ *     do~while
+ * 
+ * 
+ * 
+ * 
+ */
+
+
