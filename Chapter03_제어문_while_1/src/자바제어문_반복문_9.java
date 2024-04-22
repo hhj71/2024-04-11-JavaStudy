42954479fb915c444fa766c37d53f74554908371
hhj71 <chhj017@naver.com>
2024-04-22 20:35:12 +0900

    0422

--- Chapter03_제어문_while_1/src/자바제어문_반복문_9.java
+++ Chapter03_제어문_while_1/src/자바제어문_반복문_9.java
@@ -1,7 +1,20 @@
 /*
- * 
- * 
- * 
+ *   스프링 / 마이바티스 / JPA / 스프링 부트 
+ *    ---------------------------------
+ *    라이브러리 + 변수 + 연산자 + 제어문 
+ *              ------------------ 메소드 
+ *    while : => 데이터베이스 , 페이지나누기 
+ *                           ---------
+ *    => 반복하는 횟수를 모르는 경우에 주로 사용 
+ *       => *** for/while/do~while의 차이 
+ *    형식 => 문법 => 순서 => 응용 
+ *    형식)
+ *         초기값 
+ *         while(조건식)
+ *         {
+ *            반복 수행문장 
+ *            증가식 
+ *         }
  * 
  *    => 1부터 입력한 정수까지 합을 구해서 출력
  * 
@@ -12,9 +25,10 @@ public class 자바제어문_반복문_9 {
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 		
-		/* Runtime.getRuntime().exec("C:\Program Files\Google\Chrome\Application\\chrome.exe"
-				+)
-		*/
+		/*
+		 * Runtime.getRuntime().exec("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe "
+		 * +"http://youtube.com/embed/smdmEhkIRVc\\u0026pp=ygUJ67Ck7JaR6rCx");
+		 */
 		Scanner scan = new Scanner(System.in);
 	    System.out.print("정수 입력(1~100):");
 	    int num=scan.nextInt();
@@ -35,7 +49,7 @@ public class 자바제어문_반복문_9 {
 		{
 			sum+=i;
 		}
-		System.out.println("while: 결과값 sum="+sum);
+		System.out.println("for: 결과값 sum="+sum);
 	}
 
 }
