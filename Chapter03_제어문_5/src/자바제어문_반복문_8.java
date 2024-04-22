3afe0b0e6524b344ac2eebe5afc863badd7b6ffd
hhj71 <chhj017@naver.com>
2024-04-22 11:57:26 +0900

    0422

---
+++ Chapter03_제어문_5/src/자바제어문_반복문_8.java
@@ -0,0 +1,16 @@
+
+public class 자바제어문_반복문_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//char c = 'A';
+		// 변수의 범위지정은 {}으로 한다. => scope => 사용범위 지정을 잘해야 원하는 결과값 추출이 가능
+		for(int i=1; i<=5; i++)
+		{   
+			char c = 'A';  // for안에서 생성=> 증가 => 삭제 => 생성 반복
+			System.out.print(c+" ");
+			c++;
+		} 
+	}
+
+}
